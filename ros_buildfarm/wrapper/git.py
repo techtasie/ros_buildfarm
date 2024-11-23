45980570dd8d684f3bcae8e52f35f7273bb9ef10
Tim Wendt <tim.wendt@alumni.fh-aachen.de>
2024-11-23 12:16:46 +0100

    test2

--- ros_buildfarm/wrapper/git.py
+++ ros_buildfarm/wrapper/git.py
@@ -20,13 +20,12 @@ from time import sleep
 def main(argv=sys.argv[1:]):
     max_tries = 10
     known_error_strings = [
-        'Connection timed out',
+        "Connection timed out",
     ]
 
     command = argv[0]
-    if command == 'clone':
-        rc, _, _ = call_git_repeatedly(
-            argv, known_error_strings, max_tries)
+    if command == "clone":
+        rc, _, _ = call_git_repeatedly(argv, known_error_strings, max_tries)
         return rc
     else:
         assert "Command '%s' not implemented" % command
@@ -37,27 +36,31 @@ def call_git_repeatedly(argv, known_error_strings, max_tries):
     for i in range(1, max_tries + 1):
         if i > 1:
             sleep_time = 5 + 2 * i
-            print("Reinvoke 'git %s' (%d/%d) after sleeping %s seconds" %
-                  (command, i, max_tries, sleep_time))
+            print(
+                "Reinvoke 'git %s' (%d/%d) after sleeping %s seconds"
+                % (command, i, max_tries, sleep_time)
+            )
             sleep(sleep_time)
         rc, known_error_conditions = call_git(argv, known_error_strings)
         if rc == 0 or not known_error_conditions:
             break
-        print('')
-        print('Invocation failed due to the following known error conditions: '
-              ', '.join(known_error_conditions))
-        print('')
+        print("")
+        print(
+            "Invocation failed due to the following known error conditions: "
+            ", ".join(known_error_conditions)
+        )
+        print("")
         # retry in case of failure with known error condition
     return rc, known_error_conditions, i
 
 
 def call_git(argv, known_error_strings):
+    print("ALOLI: %s" % argv)
     known_error_conditions = []
 
-    cmd = ['git'] + argv
-    print("Invoking '%s'" % ' '.join(cmd))
-    proc = subprocess.Popen(
-        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    cmd = ["git"] + argv
+    print("Invoking '%s'" % " ".join(cmd))
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     while True:
         line = proc.stdout.readline()
         if not line:
@@ -73,5 +76,5 @@ def call_git(argv, known_error_strings):
     return rc, known_error_conditions
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
